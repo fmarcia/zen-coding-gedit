1b5aa30cb1caec22bb04cc2c5e3f8c55ee1b6b38
fmarcia <franck.marcia@gmail.com>
2010-05-11 17:21:55 +0200

    Initial commit

---
+++ zencoding/__init__.py
@@ -0,0 +1,9 @@
+'''
+Bootstrap for Zen Coding for Gedit
+
+@author Franck Marcia (franck.marcia@gmail.com)
+@link http://github.com/fmarcia/zen-coding-gedit
+'''
+
+from plugin import ZenCodingPlugin
+
