b0cbfc282fd06b05728c5e7771a792ac37c3c153
fmarcia <franck.marcia@gmail.com>
2010-05-24 01:29:57 +0200

    less mess in parsing

--- zencoding/lorem_ipsum.py
+++ zencoding/lorem_ipsum.py
@@ -26,13 +26,13 @@ Command format:
 	s[entences] [case] [number_of_words_1 [number_of_words_2 [...]]]
 	l[ist] [case] [number_of_words_1 [number_of_words_2 [...]]]
 
-	where case is { u[pper] | lo[wer] | t[itle] }
+	where case is { u[pper] | l[ower] | t[itle] | s[entence] }
 	and number_of_words is an integer or a multiplication (for example '3*6')
 	
-	commands can be shorten by using initials. For example, "wt 9" is equivalent
-	to "words title 9"
+	Commands can be shorten by using initials. For example, "wt 9" is equivalent
+	to "words title 9".
 	
-	see examples at the end of this script
+	See examples at the end of this script.
 
 @author Franck Marcia (franck.marcia@gmail.com)
 @link http://github.com/fmarcia/zen-coding-gedit
@@ -67,68 +67,63 @@ alphanumeric = [
 ]
 alphanumeric_bound = 35
 
+def to_upper(string):
+	return string.upper()
+
+def to_lower(string):
+	return string.lower()
+
+def to_title(string):
+	return string.title()
+
+def to_sentence(string):
+	return string.capitalize()
+
+def default_case(method):
+	if method in [get_characters, get_alphanumeric, get_words]:
+		return to_lower
+	elif method in [get_sentences, get_list]:
+		return to_sentence
+
 def get_characters(case, params):
 	result = ''
-	for char in range(0, params[0]):
-		char = characters[random.randint(0, characters_bound)]
-		if case == 'upper':
-			char = char.upper()
-		result += char
-	if case == 'title':
-		return result.title()
-	return result
+	for param in params:
+		for char in range(0, param):
+			result += characters[random.randint(0, characters_bound)]
+	return case(result)
 
 def get_alphanumeric(case, params):
 	result = ''
-	for char in range(0, params[0]):
-		char = alphanumeric[random.randint(0, alphanumeric_bound)]
-		if case == 'upper':
-			char = char.upper()
-		result += char
-	if case == 'title':
-		return result.title()
-	return result
+	for param in params:
+		for char in range(0, param):
+			result += alphanumeric[random.randint(0, alphanumeric_bound)]
+	return case(result)
 
 def get_words(case, params):
-	size = params[0]
-	start = 0
 	result = []
-	while True:
-		end = size if start + words_reset > size else start + words_reset
-		start_ex = random.randint(0, words_bound - end + start)
-		#print words_bound - end + start
-		#start = random.randint(0, words_bound - params[0] + 1)
-		end_ex = start_ex + end - start
-		for word in words[start_ex:end_ex]:
-			if case == 'upper':
-				result.append(word.upper())
-			elif case == 'lower':
-				result.append(word.lower())
-			elif case == 'title':
-				result.append(word.title())
-			else:
+	for size in params:
+		start = 0
+		while True:
+			end = size if start + words_reset > size else start + words_reset
+			start_ex = random.randint(0, words_bound - end + start)
+			end_ex = start_ex + end - start
+			for word in words[start_ex:end_ex]:
 				result.append(word)
-		if end == size:
-			break
-		start += words_reset
-	return ' '.join(result)
+			if end == size:
+				break
+			start += words_reset
+	return case(' '.join(result))
 
 def get_sentences(case, params):
 	result = []
 	for nbwords in params:
-		sentence = get_words(case, [nbwords])
-		if case == '':
-			sentence = sentence.capitalize()
-		result.append(sentence)
+		result.append(get_words(case, [nbwords]))
 	return '. '.join(result) + '.'
 
 def get_list(case, params):
 	result = []
 	for nbwords in params:
-		item = get_words(case, [nbwords])
-		if case == '':
-			item = item.capitalize()
-		result.append(item)
+		result.append(get_words(case, [nbwords]))
 	return '\n'.join(result)
 
 def lorem_ipsum(command):
@@ -137,50 +132,93 @@ def lorem_ipsum(command):
 	if args[0] == '':
 		return ''
 
-	def parse(args):
-		method, case, params = None, '', []
-		for arg in args:
-			arg = arg.lower()
-			if arg == '': pass
-			elif arg == 'cu': method = get_characters; case = 'upper'
-			elif arg == 'cl': method = get_characters; case = 'lower'
-			elif arg == 'ct': method = get_characters; case = 'title'
-			elif arg == 'au': method = get_alphanumeric; case = 'upper'
-			elif arg == 'al': method = get_alphanumeric; case = 'lower'
-			elif arg == 'at': method = get_alphanumeric; case = 'title'
-			elif arg == 'wu': method = get_words; case = 'upper'
-			elif arg == 'wl': method = get_words; case = 'lower'
-			elif arg == 'wt': method = get_words; case = 'title'
-			elif arg == 'su': method = get_sentences; case = 'upper'
-			elif arg == 'sl': method = get_sentences; case = 'lower'
-			elif arg == 'st': method = get_sentences; case = 'title'
-			elif arg == 'lu': method = get_list; case = 'upper'
-			elif arg == 'll': method = get_list; case = 'lower'
-			elif arg == 'lt': method = get_list; case = 'title'
-			elif 'characters'.startswith(arg): method = get_characters
-			elif 'alphanumeric'.startswith(arg): method = get_alphanumeric
-			elif 'words'.startswith(arg): method = get_words
-			elif 'sentences'.startswith(arg): method = get_sentences
-			elif 'list'.startswith(arg): method = get_list
-			elif 'upper'.startswith(arg): case = 'upper'
-			elif 'lower'.startswith(arg): case = 'lower'
-			elif 'title'.startswith(arg): case = 'title'
-			elif arg.isdigit(): params.append(int(arg))
-			else:
-				try:
-					test = arg.split('*')
-					if len(test) == 2:
-						for x in range(0, int(test[1])):
-							params.append(int(test[0]))
-				except:
-					pass
-		if len(params) == 0:
-			params = [1]
-		return method, case, params
-		
-	method, case, params = parse(args)
+	method, case, params = None, None, []
+
+	for arg in args:
+
+		arg = arg.lower()
+		if arg.isdigit():
+
+			if not method:
+				method = get_list
+
+			if not case:
+				case = default_case(method)
+
+			params.append(int(arg))
+
+		else:
+
+			test = arg.split('*')
+			if len(test) == 2 and test[0].isdigit() and test[1].isdigit():
+				if not method:
+					method = get_list
+				if not case:
+					case = default_case(method)
+				for x in range(0, int(test[1])):
+					params.append(int(test[0]))
+				continue
+
+			if len(params) == 0:
+
+				if len(arg) == 1:
+
+					if not method and not case:
+						if   arg == 'c': method = get_characters
+						elif arg == 'a': method = get_alphanumeric
+						elif arg == 'w': method = get_words
+						elif arg == 's': method = get_sentences
+						elif arg == 'l': method = get_list
+
+					elif not case:
+						if   arg == 'l': case = to_lower
+						elif arg == 'u': case = to_upper
+						elif arg == 't': case = to_title
+						elif arg == 's': case = to_sentence
+					
+				elif len(arg) == 2:
+				
+					if not method and not case:
+						if   arg == 'cu': method = get_characters; case = to_upper
+						elif arg == 'cl': method = get_characters; case = to_lower
+						elif arg == 'ct': method = get_characters; case = to_title
+						elif arg == 'au': method = get_alphanumeric; case = to_upper
+						elif arg == 'al': method = get_alphanumeric; case = to_lower
+						elif arg == 'at': method = get_alphanumeric; case = to_title
+						elif arg == 'wu': method = get_words; case = to_upper
+						elif arg == 'wl': method = get_words; case = to_lower
+						elif arg == 'wt': method = get_words; case = to_title
+						elif arg == 'su': method = get_sentences; case = to_upper
+						elif arg == 'sl': method = get_sentences; case = to_lower
+						elif arg == 'st': method = get_sentences; case = to_title
+						elif arg == 'lu': method = get_list; case = to_upper
+						elif arg == 'll': method = get_list; case = to_lower
+						elif arg == 'lt': method = get_list; case = to_title
+
+				else:
+
+					if not method and not case:
+						if   arg == 'characters': method = get_characters
+						elif arg == 'alphanumeric': method = get_alphanumeric
+						elif arg == 'words': method = get_words
+						elif arg == 'sentences': method = get_sentences
+						elif arg == 'list': method = get_list
+
+					elif not case:
+						if   arg == 'lower': case = to_lower
+						elif arg == 'upper': case = to_upper
+						elif arg == 'title': case = to_title
+						elif arg == 'sentence': case = to_sentence
+
+	if len(params) == 0:
+		params = [1]
+
 	if method is None:
 		method = get_list
+		
+	if case is None:
+		case = default_case(method)
+
 	return method(case, params)
 
 if __name__ == '__main__':
@@ -190,29 +228,44 @@ if __name__ == '__main__':
 	echo('characters')
 	echo('characters 10')
 	echo('characters upper 10')
-	echo('characters lower 12')
-	echo('characters title 14')
+	echo('characters lower 10')
+	echo('characters title 10')
+	echo('characters sentence 10')
+	echo('characters 3*2')
+	echo('characters upper 3*2')
 	echo('alphanumeric')
 	echo('alphanumeric 10')
-	echo('alphanumeric upper 16')
-	echo('alphanumeric lower 14')
-	echo('alphanumeric title 15')
-	echo('word')
-	echo('word 7')
-	echo('word upper 5')
-	echo('word lower 7')
-	echo('word title 9')
+	echo('alphanumeric upper 10')
+	echo('alphanumeric lower 10')
+	echo('alphanumeric title 10')
+	echo('alphanumeric sentence 10')
+	echo('alphanumeric 3*2')
+	echo('alphanumeric upper 3*2')
+	echo('words')
+	echo('words 3')
+	echo('words upper 3')
+	echo('words lower 3')
+	echo('words title 3')
+	echo('words sentence 3')
+	echo('words 3*2')
+	echo('words upper 3*2')
 	echo('sentences')
-	echo('sentences 5 3 9')
-	echo('sentences upper 5 3 1')
-	echo('sentences lower 4 4 7')
-	echo('sentences title 7 4 6')
+	echo('sentences 3 2')
+	echo('sentences upper 3 2')
+	echo('sentences lower 3 2')
+	echo('sentences title 3 2')
+	echo('sentences sentence 3 2')
+	echo('sentences 3*2')
+	echo('sentences upper 3*2')
 	echo('list')
-	echo('list 5')
-	echo('list 4 7')
+	echo('list 3')
+	echo('list 3 2')
 	echo('list upper 3 2')
-	echo('list lower 4 1')
-	echo('list title 2 4')
+	echo('list lower 3 2')
+	echo('list title 3 2')
+	echo('list sentence 3 2')
+	echo('list 3*2')
+	echo('list upper 3*2')
 	echo('c 10')
 	echo('c u 10')
 	echo('a 10')
